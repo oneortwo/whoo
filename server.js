48b4a1ea7353a9906af80c9e1722781ccde65fa8
Theodor Sjöstedt <theodor.sjostedt@svt.se>
2015-07-19 16:13:44 +0200

    added stuff

---
+++ server.js
@@ -0,0 +1,32 @@
+var RaspiCam = require("raspicam");
+
+var camera = new RaspiCam({ });
+
+//to take a snapshot, start a timelapse or video recording
+camera.start( );
+
+//to stop a timelapse or video recording
+//camera.stop( );
+
+//listen for the "start" event triggered when the start method has been successfully initiated
+camera.on("start", function(){
+  console.log("Camera started");
+});
+
+//listen for the "read" event triggered when each new photo/video is saved
+camera.on("read", function(err, timestamp, filename){ 
+  if(err) {
+    console.err(err);
+  }
+  console.log(timestamp + " Got image: " + filename);
+});
+
+//listen for the "stop" event triggered when the stop method was called
+camera.on("stop", function(){
+  console.log("Camera stopped");
+});
+
+//listen for the process to exit when the timeout has been reached
+camera.on("exit", function(){
+    console.log("Camera exit");
+});
